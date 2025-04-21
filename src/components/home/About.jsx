ec57efa1c180e8fe9407afcc5ad63d4962a8149c
macbook <jamaspishvilinika@icloud.com>
2025-04-21 04:10:50 +0400

    commit

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div id="about" className="scroll-mt-20 flex flex-col md:flex-row items-start gap-10 p-5">
+    <div id="about" className="scroll-mt-20 flex mt-16 flex-col md:flex-row items-start gap-10 p-5">
       <img src="/collage.jpg" className="md:w-1/2 w-full h-[500px] max-md:order-2" alt="collage (about) photo" />
       <section className="md:w-1/2 w-full text-center md:text-left flex flex-col gap-8 mt-10 max-md:order-1">
       <h2 className="text-5xl">ვინ ვართ ჩვენ?</h2>
